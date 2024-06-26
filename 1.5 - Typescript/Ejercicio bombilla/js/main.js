fad0224eeacc7e001bca07a6740733fc6df18016
Xavi Campos <daxterxavi@gmail.com>
2024-06-26 16:00:13 +0200

    Ejercicio de la bombilla

--- 1.5 - Typescript/Ejercicio bombilla/js/main.js
+++ 1.5 - Typescript/Ejercicio bombilla/js/main.js
@@ -1,24 +1,22 @@
+"use strict";
 // función que coje los cliccs 
 document.addEventListener("DOMContentLoaded", function () {
     let interruptor = document.getElementById("interruptor");
-    interruptor?.addEventListener("click", cambiarInterruptor);
+    interruptor === null || interruptor === void 0 ? void 0 : interruptor.addEventListener("click", cambiarInterruptor);
 });
-
-
-function cambiarInterruptor(event: MouseEvent) {
-    let cambiar = event.target as HTMLImageElement;
-    let src = cambiar.src.split("/").pop(); 
-    let bombilla  = document.getElementById("Bombilla") as HTMLImageElement;
-
-
+function cambiarInterruptor(event) {
+    let cambiar = event.target;
+    let src = cambiar.src.split("/").pop();
+    let bombilla = document.getElementById("Bombilla");
     if (src === "boff.jpg") {
         cambiar.src = "./img/bon.jpg";
-        bombilla.src  = "./img/on.jpg";
+        bombilla.src = "./img/on.jpg";
         console.log(cambiar.src);
         console.log("entra on");
-    } else {
+    }
+    else {
         cambiar.src = "./img/boff.jpg";
-        bombilla.src  = "./img/off.jpg";
+        bombilla.src = "./img/off.jpg";
         console.log(cambiar.src);
         console.log("entra off");
     }
